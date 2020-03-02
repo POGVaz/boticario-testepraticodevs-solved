d7ee3dae659b67fe39093b5fcee0d83f6d2aecc2
Pedro Oscar <pedro.oscar.vaz@usp.br>
2020-03-02 01:03:24 -0300

    Constructed views

--- boticarioBackend/retailer_cashback/urls.py
+++ boticarioBackend/retailer_cashback/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('register-retailer', views.register_retailer, name='register_retailer'),
+    path('validate-retailer', views.validate_retailer, name='validate_retailer'),
+    path('register-purchase', views.register_purchase, name='register_purchase'),
+    path('list-purchases', views.list_purchases, name='list_purchases'),
+    path('accumulated-cashback', views.accumulated_cashback, name='accumulated_cashback'),
+
+    path('get-token-auth/', obtain_jwt_token , name='get_token'),
+    path('refresh-token-auth/', refresh_jwt_token, name='refresh_token'),
+    path('verify-token-auth/', verify_jwt_token, name='verify_token'),
 ]
--- boticarioBackend/retailer_cashback/views.py
+++ boticarioBackend/retailer_cashback/views.py
@@ -1,9 +1,67 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from django.contrib.auth import authenticate
+
+from retailer_cashback import models, serializers, services
 
 import logging
 logger = logging.getLogger(__name__)
 
-def index(request):
-    pass
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def register_retailer(request):
+    request_data = {
+        'email': request.POST.get('email'),
+        'password': request.POST.get('password'),
+        'first_name': request.POST.get('first_name'),
+        'last_name': request.POST.get('last_name'),
+        'cpf': request.POST.get('cpf'),
+    }
 
-# Create your views here.
+    created_retailer = services.create_retailer(**request_data)
+    logger.info("Created new retailer {}".format(created_retailer.user.username))
+    return Response("Registered {}".format(created_retailer.user.username))
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def validate_retailer(request):
+    user = authenticate(username=request.GET.get('username'),
+                        password=request.GET.get('password'))
+
+    if user is not None:
+        return Response("User {} is Valid".format(user.username))
+    else:
+        logger.info("User {} is Invalid".format(
+            request.GET.get('username')))
+        response = Response("User {} is Invalid".format(
+            request.GET.get('username')))
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return response
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def register_purchase(request):
+    request_data = {
+        'code': request.POST.get('code'),
+        'purchase_date': request.POST.get('date'),
+        'cpf': request.POST.get('cpf'),
+        'value': request.POST.get('value'),
+    }
+    # Creates a purchase with all the business logic required
+    purchase = services.create_purchase(**request_data)
+    return Response("Purchase registered: {}".format(purchase.code))
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def list_purchases(request):
+    queryset = models.Purchase.objects.order_by('-code')
+    serializer = serializers.PurchaseSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def accumulated_cashback(request):
+    return Response(services.get_accumulated_cashback(request.GET['cpf']))
